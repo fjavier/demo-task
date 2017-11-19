e704fa4ec8608a023b63c9ce3a6814c75a9027d5
fbriceno <fjavier.cgam@gmail.com>
2017-11-18 22:47:12 -0600

    Populating database.
    Populating database with experimental tasks when the application start

--- src/main/java/com/fbriceno/task/TaskApplication.java
+++ src/main/java/com/fbriceno/task/TaskApplication.java
@@ -1,7 +1,13 @@
 package com.fbriceno.task;
 
+import com.fbriceno.task.entity.Task;
+import com.fbriceno.task.service.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TaskApplication {
@@ -9,4 +15,13 @@ public class TaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner(TaskService taskService){
+	    return args -> {
+	        taskService.save(new Task(1L, "SLEEP", LocalDate.now(), true));
+            taskService.save(new Task(2L, "AWAKE", LocalDate.now(), false));
+            taskService.save(new Task(3L, "EAT", LocalDate.now(), false));
+        };
+    }
 }
