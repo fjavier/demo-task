70c7513d1b91b45c793c4ce7d4642e415f210240
fbriceno <fjavier.cgam@gmail.com>
2017-11-20 19:31:24 -0600

    Showing lists of tasks.
    The tasks were created with typescript this is like the backBean where
    we have our variables and functions.
    The task list is an array of type Task.
    in the view I show the list of tasks iteratign the array of Tasks

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from "../tasks.model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+    tasks:Task[] = [];
 
   constructor() { }
 
   ngOnInit() {
+      this.tasks.push(new Task(1,"Task 1", true, "01/01/2018"));
+      this.tasks.push(new Task(2,"Task 2", true, "02/01/2018"));
+      this.tasks.push(new Task(3,"Task 3", false, "03/01/2018"));
+
+  }
+
+  getDueDateLabel(task: Task){
+        return task.completed ? 'label-success' : 'label-primary';
+  }
+
+  get
+
+  onTaskChange(event, task){
+      console.log("Task has changed");
   }
 
 }
